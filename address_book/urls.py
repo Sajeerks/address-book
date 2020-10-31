cb9ef22114bdbdefb0d7ee42d3bcdf16e0276315
sajeer <sajeersayed@gmail.com>
2020-10-31 14:12:59 +0300

    did all

--- address_book/urls.py
+++ address_book/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
 
 path('', views.home, name = 'home'),
 path('add_address', views.add_address, name = 'add_address'),
-    
+ path('edit/<list_id>', views.edit, name = 'edit'),
+  path('delete/<list_id>', views.delete, name = 'delete'),
 ]
